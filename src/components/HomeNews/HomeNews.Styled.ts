1ab437ded0e3d3d96f97dfae9fa663859db211bf
Renan Cesar <cesar.renan@hotmail.com>
2023-07-24 11:05:29 -0300

    feat: começando componente HomeNews

--- src/components/HomeNews/HomeNews.Styled.ts
+++ src/components/HomeNews/HomeNews.Styled.ts
@@ -4,28 +4,62 @@ const comum = `
 display: flex;
 width: 100%;
 height: 100%;
-`
+`;
 
 export const Container = styled.div`
   padding: var(--padding-geral);
   height: 600px;
   display: flex;
   flex-direction: column;
-  
+
   & .container-geral {
     ${comum}
     margin-bottom: 10px;
-    
-    & .noticia-maior {
-        ${comum}
-        padding: 30px;
-    }
-    
+    gap: 30px;
+
+    & .noticia-maior,
     & .outras-noticias {
-        padding: 30px;
-        ${comum}        
+      ${comum}
+      padding: 30px 30px 0px 30px;
     }
-}
+
+    & .noticia-maior {
+      display: flex;
+      flex-direction: column;
+      background-color: red;
+
+      & .link-container {
+        text-decoration: none;
+        color: var(--preto-fonte);
+
+        & .img-container {
+          width: 100%;
+  
+          & img {
+            width: 100%;
+            height: 300px;
+          }
+        }
+  
+        & .categoria-container {
+          display: flex;
+          gap: 5px;
+          margin: 10px 0px;
+  
+          & .item {
+            border: 1px solid var(--border-color);
+            border-radius: 50px;
+            padding: 1px 10px;
+          }
+        }
+      }
+
+    }
+
+    & .outras-noticias {
+      background-color: green;
+    }
+  }
 
   & .mais-noticiais {
     display: flex;
@@ -35,10 +69,10 @@ export const Container = styled.div`
     cursor: pointer;
 
     & p {
-        border: 1px solid var(--border-color);
-        font-weight: bold;
-        border-radius: 50px;        
-        padding: 1px 10px;
+      border: 1px solid var(--border-color);
+      font-weight: bold;
+      border-radius: 50px;
+      padding: 1px 10px;
     }
-}
+  }
 `;
--- src/components/HomeNews/index.tsx
+++ src/components/HomeNews/index.tsx
@@ -1,14 +1,43 @@
 import { Container } from "./HomeNews.Styled";
+import Post from "../../utils/post.json"
+import { Link } from "react-router-dom";
+import Like from "../Like";
 
 const HomeNews = () => {
   return (
     <Container>
       <div className="container-geral">
         <div className="noticia-maior">
-          
+          <Link to="/noticia" className="link-container">
+            <div className="img-container">
+              <img src={Post.banner} alt="foto-notícia" />
+            </div>
+            <div className="categoria-container">
+              {
+                Post.tags.map((i: string) =>
+                  <div key={i} className='item'>
+                    {i}
+                  </div>
+                )
+              }
+            </div>
+            <div className="title-container">
+              <h1>
+                {Post.title}
+              </h1>
+            </div>
+            <div className="status-container">
+              <div className="like">
+              <Like likes={Post.likes} />
+              </div>
+              <div className="user-container">
+
+              </div>
+            </div>
+          </Link>
         </div>
         <div className="outras-noticias">
-          
+
         </div>
       </div>
       <div className="mais-noticiais">
