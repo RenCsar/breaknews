8c9a69af3f683d21612d0b7b33569cc7fea2960e
Renan Cesar <cesar.renan@hotmail.com>
2023-08-16 19:26:11 -0300

    refac: refatorando estilos de cadastroForm

---
+++ src/components/CadastroForm/CadastroForm.Styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import { Container as CommonStyles } from "../LoginForms/LoginForms.Styled";
+
+export const Container = styled.div`
+  ${CommonStyles}
+`;
